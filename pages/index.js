8380b03defcb0e7e54eb7e784c0bb7510b4bc309
akshaysable02 <akshaysable097@gmail.com>
2024-01-19 14:59:44 +0530

    Making the Details page work properly

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
 import { MongoClient } from "mongodb";
 import MeetupList from "@/components/meetups/MeetupList";
 
-const DUMMY_MEETUP_DATA = [
-  {
-    id:'m1',
-    title:'A First  Meetup',
-    image:'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg',
-    address:'Some address',
-    description:'This is first meetup',
-  },
-  {
-    id:'m2',
-    title:'A Second  Meetup',
-    image:'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg',
-    address:'Some address',
-    description:'This is second meetup',
-  }
-]
+
 
 function HomePage(props){
   return  <MeetupList meetups={props.meetups} />
